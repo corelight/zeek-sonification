48c75e2e0242863e6aae742466b1823e2738a2d2
Ryan Victory <github@beerandraptors.com>
2020-04-22 16:01:15 -0500

    Some better methods for keeping track of the min and max TS, along with a better algorithm for determining amplitude

--- zeek_to_audio.rb
+++ zeek_to_audio.rb
@@ -11,22 +11,31 @@ end
 puts "Loading the conn file"
 
 timestamps = {}
+max_value = 0
+min_ts = 99999999999
+max_ts = 0
+
 ARGF.each_line do |line|
   next if line[0] == '#'
   parts = line.split("\t")
   timestamp = parts[0].to_i
+
+  min_ts = timestamp if timestamp < min_ts
+  max_ts = timestamp if timestamp > max_ts
+
   timestamps[timestamp] ||= 0
   timestamps[timestamp] += 1
+  max_value = timestamps[timestamp] if timestamps[timestamp] > max_value
 end
 
-puts "Generating Audio"
-
-sorted_keys = timestamps.keys.sort
-min_ts = sorted_keys.first
-max_ts = sorted_keys.last
 
 puts "Minimum timestamp in your conn.log: #{min_ts}"
 puts "Maximum timestamp in your conn.log: #{max_ts}"
+puts "Maximum amplitude in your conn.log: #{max_value}"
+
+puts "Generating Audio"
+# Our max volume is 1 for audio output. Let's use 0.9 to make sure we don't have any volume issues
+volume_step_factor = 0.9 / max_value
 
 num_timestamps = max_ts - min_ts
 time_length = (num_timestamps * 1.0) / sample_rate
@@ -40,7 +49,7 @@ file.puts "; Sample Rate #{sample_rate}"
 file.puts "; Channels 1"
 
 min_ts.upto(max_ts) do |x|
-  file.puts "\t#{(x - min_ts) * steps_per_timestamp}\t#{(timestamps[x] || 0) * 0.04}"
+  file.puts "\t#{(x - min_ts) * steps_per_timestamp}\t#{(timestamps[x] || 0) * volume_step_factor}"
 end
 file.close
 
